74bd8031d19f58472d27e403e5c7dc7c05ccab3e
Alejandro Breen Herrera <31518701+some-tortoise@users.noreply.github.com>
2023-11-30 13:04:21 -0500

    Binarization should work well now

--- fingerprint.py
+++ fingerprint.py
@@ -27,7 +27,7 @@ def Arr2DToBW(img, thresh = 0.95):
     
     return im
 
-def BinarizeFingerprint(imageName, method = "Pixel Blocks", blockSize = 8):
+def BinarizeFingerprint(imageName, method = "Pixel Blocks", blockSize = 9, thresh = 0.01):
     BnW_image = []
 
     img = cv2.imread(imageName, cv2.IMREAD_ANYCOLOR)
@@ -44,28 +44,18 @@ def BinarizeFingerprint(imageName, method = "Pixel Blocks", blockSize = 8):
         im = To2DArr(img)
         BnW_image = np.zeros((len(im), len(im[0])))
         j = 0
-        while j+blockSize-1 < len(im[0]):
+        while j < len(im[0]):
             i = 0
-            while i+blockSize-1 < len(im):
-                mean = np.mean([np.mean(im[i+x][j:j+blockSize-1]) for x in range(0,blockSize)])
-                BnW_image[i+math.floor(blockSize/2)][j+math.floor(blockSize/2)] = im[i+math.floor(blockSize/2)][j+math.floor(blockSize/2)] >= mean
+            while i < len(im):
+                half = math.floor(blockSize/2)
+                rangeJ = range(max(j-half, 0), min(j+half, len(im[0])-1))
+                rangeI = range(max(i-half, 0), min(i+half, len(im)-1))
+                mean = np.mean([np.mean([im[x][y] for y in rangeJ]) for x in rangeI])
+                BnW_image[i][j] = im[i][j] > mean - thresh
                 i += 1
             j += 1
 
         # CREDIT TO https://ieeexplore.ieee.org/document/1716119
-    elif method == 'Scharr + Pixel Blocks':
-        edge_scharr = filters.scharr(img)
-        img = invert(edge_scharr)
-        im = To2DArr(img)
-        BnW_image = np.zeros((len(im), len(im[0])))
-        j = 0
-        while j+blockSize-1 < len(im[0]):
-            i = 0
-            while i+blockSize-1 < len(im):
-                mean = np.mean([np.mean(im[i+x][j:j+blockSize-1]) for x in range(0,blockSize)])
-                BnW_image[i+math.floor(blockSize/2)][j+math.floor(blockSize/2)] = im[i+math.floor(blockSize/2)][j+math.floor(blockSize/2)] >= mean
-                i += 1
-            j += 1
     elif method == 'None': # USE PIXEL BY PIXEL THRESHOLD
         (thresh, BnW_image) = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
     else:
