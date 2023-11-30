5c6dd9e7d053b81fde2093c4acbfca23e9c82984
Alejandro Breen Herrera <31518701+some-tortoise@users.noreply.github.com>
2023-11-30 12:00:55 -0500

    A

--- file.py
+++ file.py
@@ -26,7 +26,7 @@ def Arr2DToBW(img, thresh = 0.95):
     
     return im
 
-def BinarizeFingerprint(imageName, method = "None", blockSize = 8):
+def BinarizeFingerprint(imageName, method = "Pixel Blocks", blockSize = 8):
     BnW_image = []
 
     img = cv2.imread(imageName, cv2.IMREAD_ANYCOLOR)
@@ -52,6 +52,19 @@ def BinarizeFingerprint(imageName, method = "None", blockSize = 8):
             j += 1
 
         # CREDIT TO https://ieeexplore.ieee.org/document/1716119
+    elif method == 'Scharr + Pixel Blocks':
+        edge_scharr = filters.scharr(img)
+        img = invert(edge_scharr)
+        im = To2DArr(img)
+        BnW_image = np.zeros((len(im), len(im[0])))
+        j = 0
+        while j+blockSize-1 < len(im):
+            i = 0
+            while i+blockSize-1 < len(im[0]):
+                mean = np.mean([np.mean(im[i+x][j:j+blockSize-1]) for x in range(0,blockSize)])
+                BnW_image[i+math.floor(blockSize/2)][j+math.floor(blockSize/2)] = im[i+math.floor(blockSize/2)][j+math.floor(blockSize/2)] >= mean
+                i += 1
+            j += 1
     elif method == 'None': # USE PIXEL BY PIXEL THRESHOLD
         (thresh, BnW_image) = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
     else:
